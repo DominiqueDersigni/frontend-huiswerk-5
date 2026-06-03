81636dbf35621ecaea0e1482a7a042ca06ba4a3b
dominiquedersigni <dominiquedersigni@gmail.com>
2026-06-03 21:12:24 +0200

    opdracht-2

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,18 @@ console.log("Marketing is een leuke afdeling om te werken. " + departments.marke
 console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers")
 console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager." + departments.sales.jobs[1].description)
 
+const userInput = prompt('Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]');
+console.log(userInput);
+
+if (userInput === 'marketing') {
+    console.log("Je koos marketing. " + departments.marketing.description)
+}
+else if (userInput === 'sales') {
+    console.log("Je koos sales. " + departments.sales.description)
+}
+else if (userInput === 'customer-service') {
+    console.log("Je koos customer-service. " + departments["customer-service"].description)
+}
+else {
+    console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+}
